ab620cc8174dff2770abf1eda854578442344b37
stephanybatista <stephany.batista@outlook.com>
2014-06-09 13:20:23 -0400

    Commit initial

---
+++ SampleWebSignalR/SampleHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SampleWebSignalR
+{
+    public class SampleHub : Hub
+    {
+        public SampleHub()
+        {
+
+        }
+
+        public override Task OnConnected()
+        {
+            Clients.All.entryClient(Context.ConnectionId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected()
+        {
+ 	        Clients.All.exitClient(Context.ConnectionId); 
+            return base.OnDisconnected();
+        }
+
+        public string WellCome()
+        {
+            return "Bem vindo";
+        }
+    }
+}
---
+++ SampleWebSignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SampleWebSignalR.Startup))]
+namespace SampleWebSignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
